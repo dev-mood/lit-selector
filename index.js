fcc26a6cd8a154552b52faf95da9f9e038eba821
Roberth <roberthgon18@gmail.com>
2020-02-19 17:37:23 +0100

    Add 'ls' method to HTMLElement interface.

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+/**
+ * LitSelector
+ * @param { DOMString } target
+ */
 function ls(target) {
-  let selected = document.querySelectorAll(target);
-  if (selected.length > 1) return Array.from(selected);
+  let selected;
+
+  // return undefined if the target is empty
+  if (target.length === 0) return;
+
+  /**
+   * Add "ls" method to HTMLElement interface.
+   *
+   * Now we can use parentNode.ls("target")
+   * to find a DOM element.
+   */
+  HTMLElement.prototype.ls = ls;
+
+  if (this instanceof HTMLElement) {
+    selected = this.querySelectorAll(target);
+  } else {
+    // parentNode by default is "document"
+    selected = document.querySelectorAll(target);
+  }
+
+  if (selected.length > 1) return selected;
+
   return selected[0];
-};
+}
